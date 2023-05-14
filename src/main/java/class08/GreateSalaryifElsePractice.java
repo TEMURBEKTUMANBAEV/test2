c5e3f8b04b6c14b28f1c2ac0f16921dc04cfdb11
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-14 19:53:41 +0300

    Update GreateSalaryifElsePractice.java

--- src/main/java/class08/GreateSalaryifElsePractice.java
+++ src/main/java/class08/GreateSalaryifElsePractice.java
@@ -1,4 +1,42 @@
 package class08;
+import java.util.Scanner;
 
 public class GreateSalaryifElsePractice {
+    public static void main(String[] args) {
+        /**
+         * Write a program to get the salaries of two-people
+         * and print the name of the person that has a greater salary
+         * Question Type: Write Code.
+         *
+         * Test Data:
+         * Example 1
+         * Person 1: 1200
+         * Person 2: 2500
+         * Output: 2500
+         *
+         * Example 2:
+         * Person 1: 4500
+         * Person 2: 8600
+         * Output: 8600
+         */
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please Enter Person 1 salary");
+        double salary1 = input.nextDouble();
+
+        System.out.println("Please Enter Person 2 salary");
+        double salary2 = input.nextDouble();
+
+        if (salary1 > salary2) {
+            System.out.println(salary1);
+        }
+        else  {
+            System.out.println(salary2);
+        }
+
+
+
+    }
+
 }
