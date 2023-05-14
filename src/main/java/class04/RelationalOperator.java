bcd03da972a38a2d3e9da5769bd09b7006128476
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-14 19:37:23 +0300

    Update RelationalOperator.java

--- src/main/java/class04/RelationalOperator.java
+++ src/main/java/class04/RelationalOperator.java
@@ -1,4 +1,14 @@
 package class04;
 
 public class RelationalOperator {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 11;
+
+        System.out.println(num1 < num2);  // true
+        System.out.println(num1 > num2);  // false
+        System.out.println(num1 <= num2); // true
+        System.out.println(num1 >= num2); // false
+        System.out.println(num1 != num2); // true
+        System.out.println(num1 == num2); // false
+    }
 }
