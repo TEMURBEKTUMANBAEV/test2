88636fc264d35973f46a58f28444bdb619b1f65d
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-14 20:33:38 +0300

    Update WeDevX.java

--- src/main/java/class12/WeDevX.java
+++ src/main/java/class12/WeDevX.java
@@ -1,4 +1,35 @@
 package class12;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class WeDevX {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String line1;
+        String line2;
+
+        double total = 0;
+
+        if ((line1 = reader.readLine()) != null) {
+            total = Double.parseDouble(line1);
+        }
+
+        double cashReceived = 0;
+
+        if ((line2 = reader.readLine()) != null) {
+            cashReceived = Double.parseDouble(line2);
+        }
+
+        System.out.println(Solution.cashier(total, cashReceived));
+    }
+}
+
+
+class Solution {
+    public static double cashier(double total, double cashReceived) {
+        return total - cashReceived;
+    }
 }
