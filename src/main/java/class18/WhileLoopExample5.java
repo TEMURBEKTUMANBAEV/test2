8eca23858c65644b09dbc6c98d747acb813fcf6f
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-16 09:27:44 +0300

    Update WhileLoopExample5.java

--- src/main/java/class18/WhileLoopExample5.java
+++ src/main/java/class18/WhileLoopExample5.java
@@ -1,4 +1,15 @@
 package class18;
 
 public class WhileLoopExample5 {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 5) {
+            i++;
+            System.out.println(i);
+            i++;
+        }
+
+    }
 }
