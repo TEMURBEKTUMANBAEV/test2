8371a0deaca7011c7221eaf39a15289d8f868755
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-16 09:29:18 +0300

    Update Factorial.java

--- src/main/java/class19/Factorial.java
+++ src/main/java/class19/Factorial.java
@@ -1,4 +1,13 @@
 package class19;
 
 public class Factorial {
+    public static void main(String[] args) {
+        int sum = 1;
+
+        for (int i = 1; i <= 5; i++) {
+            sum = sum * i;
+        }
+
+        System.out.println(sum);
+    }
 }
