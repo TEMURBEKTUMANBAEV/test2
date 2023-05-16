874ba3899babccc8c2f2e82d6e69d46406faf2af
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-16 09:26:23 +0300

    Update WhileLoopExample.java

--- src/main/java/class18/WhileLoopExample.java
+++ src/main/java/class18/WhileLoopExample.java
@@ -1,4 +1,17 @@
 package class18;
 
 public class WhileLoopExample {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 50) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                System.out.println(i);
+            }
+            i++;
+        }
+
+
+    }
 }
