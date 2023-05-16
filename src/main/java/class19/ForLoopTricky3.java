9ece7741dafb0591d26685d4bff32422232260b0
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-16 09:31:42 +0300

    Update ForLoopTricky3.java

--- src/main/java/class19/ForLoopTricky3.java
+++ src/main/java/class19/ForLoopTricky3.java
@@ -1,4 +1,15 @@
 package class19;
 
 public class ForLoopTricky3 {
+    public static void main(String[] args) {
+
+        int i;
+
+        for (i = 1; i <= 10; i++) {
+            i = i + 2;
+
+        }
+        System.out.println(i);
+
+    }
 }
