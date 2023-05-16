10b9d91122d5b58a6668eca68f69e91d25ee3a11
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-16 09:32:02 +0300

    Update SumOf1to5.java

--- src/main/java/class19/SumOf1to5.java
+++ src/main/java/class19/SumOf1to5.java
@@ -1,4 +1,13 @@
 package class19;
 
 public class SumOf1to5 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            sum = sum + i;
+        }
+
+        System.out.println(sum);
+    }
 }
