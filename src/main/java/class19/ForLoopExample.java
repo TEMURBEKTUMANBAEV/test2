99e662f78485815da937e58755b9bdea29c9fd8b
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-16 09:30:38 +0300

    Update ForLoopExample.java

--- src/main/java/class19/ForLoopExample.java
+++ src/main/java/class19/ForLoopExample.java
@@ -1,4 +1,21 @@
 package class19;
 
 public class ForLoopExample {
+    public static void main(String[] args) {
+        int i = 1; // initialization
+
+        while (i <= 5) { // Condition
+            System.out.println(i);
+            i++; // Increment / Decrement
+        }
+
+        for (int j = 1; j <= 5; j++) {
+            System.out.println(j);
+        }
+
+        for (int j = 10; j >= 1; j--) {
+            System.out.println(j);
+        }
+
+    }
 }
