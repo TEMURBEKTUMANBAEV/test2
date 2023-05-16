704b943cd2a92d3979025c78ebb17ac099f3bdd8
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-16 09:21:28 +0300

    Update Tim.java

--- src/main/java/class15/Tim.java
+++ src/main/java/class15/Tim.java
@@ -1,4 +1,26 @@
 package class15;
 
 public class Tim {
+    public static void main(String[] args) {
+        String str = "Elon ";
+        boolean isEqual = str.equals("Elon");
+        System.out.println(isEqual);
+
+        String answer = "                               yes                        ";
+        boolean yesOrNo = answer.equalsIgnoreCase("yes");
+        System.out.println(yesOrNo);
+
+        System.out.println(answer.trim());//return yes without the space
+
+
+        //method chaining only if the method return a String
+        switch (answer.toLowerCase().trim()) {}
+
+        if(answer.trim().equalsIgnoreCase("elon musk")) { }
+
+
+
+
+
+    }
 }
