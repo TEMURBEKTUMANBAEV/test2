1e1812a97f397e9a046f02ccc2c83b1f24a87a7f
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-16 09:27:08 +0300

    Update WhileLoopExample3.java

--- src/main/java/class18/WhileLoopExample3.java
+++ src/main/java/class18/WhileLoopExample3.java
@@ -1,4 +1,14 @@
 package class18;
 
 public class WhileLoopExample3 {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i < 5) {
+            System.out.println(i);
+            i = i + 1;
+        }
+
+    }
 }
