9c3d91eb2fb8788ca88814969a95a92d6054431c
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-16 09:24:10 +0300

    Update WhileLoopPractice4.java

--- src/main/java/class17/WhileLoopPractice4.java
+++ src/main/java/class17/WhileLoopPractice4.java
@@ -1,4 +1,26 @@
 package class17;
 
 public class WhileLoopPractice4 {
+    public static void main(String[] args) {
+        //write program that prints out
+        //every other character of a String
+
+        //ex: Java
+        //a a
+        //Hello
+        //e l
+        //World
+        //o l
+        //Mission Impossible
+        //i s o  m o s i l
+
+        String str = "Hello World from Mars";
+
+        int i = 1;
+
+        while (i < str.length()) {
+            System.out.println(str.charAt(i));
+            i+=2;
+        }
+    }
 }
