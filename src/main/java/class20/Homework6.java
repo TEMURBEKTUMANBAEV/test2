0d696ad7c35277eeb7e4b5e4e590438ed40bb8a1
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-16 09:33:42 +0300

    Update Homework6.java

--- src/main/java/class20/Homework6.java
+++ src/main/java/class20/Homework6.java
@@ -1,4 +1,14 @@
 package class20;
+// Java program to find sum of 1^2 + 2^2 + 3^2 + 4^2 + 5^2
 
 public class Homework6 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            sum = sum + i * i;
+        }
+
+        System.out.println(sum);
+    }
 }
