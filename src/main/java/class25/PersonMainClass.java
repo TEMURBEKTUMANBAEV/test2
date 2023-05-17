8407331bc94989a3183f3563d2b60bcd92847b9f
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-17 09:14:57 +0300

    Update PersonMainClass.java

--- src/main/java/class25/PersonMainClass.java
+++ src/main/java/class25/PersonMainClass.java
@@ -1,4 +1,28 @@
 package class25;
 
 public class PersonMainClass {
+    public static void main(String[] args) {
+        // Create reference of class
+        Person aidai;
+
+        // Initialize memory for the object
+        Person dana = new Person();
+        Person azat = new Person();
+
+        dana.age = 34;
+        azat.age = 14;
+
+        dana.name = "Danaker";
+        azat.name = "Azat Azamatov";
+
+        dana.run();
+        azat.eat();
+
+
+
+
+
+
+    }
+
 }
