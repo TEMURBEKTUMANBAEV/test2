4380316d20766bf4cf80ae691d6cb56479d45cb6
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-17 09:13:08 +0300

    Update Bank.java

--- src/main/java/class25/Bank.java
+++ src/main/java/class25/Bank.java
@@ -1,4 +1,16 @@
 package class25;
 
 public class Bank {
+    boolean isSavingAccount;
+    double balance;
+    String accName;
+
+    void deposit(double amount) {
+        balance = balance + amount;
+    }
+
+    void withdraw(double amount) {
+        balance = balance - amount;
+    }
+
 }
