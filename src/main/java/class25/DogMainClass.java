9e5506c4558fa7ab2777ef1e95535c424fa31a54
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-17 09:14:20 +0300

    Update DogMainClass.java

--- src/main/java/class25/DogMainClass.java
+++ src/main/java/class25/DogMainClass.java
@@ -1,4 +1,17 @@
 package class25;
 
 public class DogMainClass {
+    public static void main(String[] args) {
+        Dog sharik = new Dog("sharik", 15);
+
+        Dog jhonty = new Dog();
+
+        Dog rani = new Dog();
+
+        Dog lessie = new Dog("lessie", 12);
+
+        sharik.bark();
+        jhonty.bark();
+    }
+
 }
