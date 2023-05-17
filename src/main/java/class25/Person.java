d7b5ba0b6f32161bf321f0cef197679da62d3faf
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-17 09:14:39 +0300

    Update Person.java

--- src/main/java/class25/Person.java
+++ src/main/java/class25/Person.java
@@ -1,4 +1,19 @@
 package class25;
 
 public class Person {
+    // Variables
+    String name;
+    int age;
+    char gender;
+    double salary;
+
+    // Methods
+    void eat() {
+        System.out.println(name + " is eating");
+    }
+
+    void run() {
+        System.out.println(name + " is running");
+    }
+
 }
