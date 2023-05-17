824c0274a60c404a6747fccff720b120ada37950
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-17 09:13:26 +0300

    Update BankMainCLass.java

--- src/main/java/class25/BankMainCLass.java
+++ src/main/java/class25/BankMainCLass.java
@@ -1,4 +1,22 @@
 package class25;
 
 public class BankMainCLass {
+    public static void main(String[] args) {
+        Bank tairaAccount = new Bank();
+        Bank zarinaAccount = new Bank();
+
+        tairaAccount.balance = 1000;
+        tairaAccount.accName = "Taira";
+
+        zarinaAccount.balance = 5000;
+        zarinaAccount.accName = "Zarina";
+
+        tairaAccount.withdraw(6000000);
+        System.out.println(tairaAccount.balance);
+
+        System.out.println(zarinaAccount.balance);
+
+
+    }
+
 }
