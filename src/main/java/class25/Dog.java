780e75b0c08116a33e4dff0ff076cd66b45982e0
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-17 09:13:59 +0300

    Update Dog.java

--- src/main/java/class25/Dog.java
+++ src/main/java/class25/Dog.java
@@ -1,4 +1,24 @@
 package class25;
 
 public class Dog {
+    String name;
+    int age;
+    char gender;
+
+    Dog(String nameFrmUser, int ageFromUser){
+        name = nameFrmUser;
+        age = ageFromUser;
+    }
+
+    Dog(){
+
+    }
+
+    void bark() {
+        System.out.println(name + " is barking");
+    }
+
+    void play() {
+        System.out.println(name + " is playing");
+    }
 }
