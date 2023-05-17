b5da37768e21c25b0c5f79d940b21653c4bec0da
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-17 08:46:42 +0300

    Update NestedForLoopProgram2.java

--- src/main/java/class21/NestedForLoopProgram2.java
+++ src/main/java/class21/NestedForLoopProgram2.java
@@ -1,4 +1,15 @@
 package class21;
 
 public class NestedForLoopProgram2 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+
+            for(int s=1; s<=5-i; s++){
+
+            }
+            System.out.println("Hello");
+        }
+
+    }
 }
