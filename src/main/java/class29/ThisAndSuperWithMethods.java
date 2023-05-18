c8dcd54c3830fb2305ceb732ea5ac3216458d2b6
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-18 21:46:10 +0300

    Update ThisAndSuperWithMethods.java

--- src/main/java/class29/ThisAndSuperWithMethods.java
+++ src/main/java/class29/ThisAndSuperWithMethods.java
@@ -1,4 +1,19 @@
 package class29;
+class Bank{
+    void displayRateOfInterest(){
+        System.out.println("4.0%");
+    }
+}
 
+class ChaseBank extends Bank{
+    void displayRateOfInterest(){
+        System.out.println("6.0%");
+        super.displayRateOfInterest();
+    }
+}
 public class ThisAndSuperWithMethods {
+    public static void main(String[] args) {
+        ChaseBank cb = new ChaseBank();
+        cb.displayRateOfInterest();
+    }
 }
