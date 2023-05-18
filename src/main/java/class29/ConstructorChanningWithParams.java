e4c85cc776bfbae8bfc30fa4a559d62b76caecdb
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-18 21:44:53 +0300

    Update ConstructorChanningWithParams.java

--- src/main/java/class29/ConstructorChanningWithParams.java
+++ src/main/java/class29/ConstructorChanningWithParams.java
@@ -1,4 +1,26 @@
 package class29;
+class Animal{
+    Animal(String name){
+        System.out.println("From Animal "+name);
+    }
+}
+
+class Dog extends Animal{
+    Dog(String name){
+        super("abc");
+        System.out.println("From Dog " + name);
+    }
+}
+
+class Pets extends Dog{
+    Pets(){
+        super("Jhonty");
+        System.out.println("From Pets");
+    }
+}
 
 public class ConstructorChanningWithParams {
+    public static void main(String[] args) {
+        Pets pet = new Pets();
+    }
 }
