37e680f15b799aa9ca7dc078624666e171b99f94
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-18 21:40:59 +0300

    Update BankMainClass.java

--- src/main/java/class26/BankMainClass.java
+++ src/main/java/class26/BankMainClass.java
@@ -1,4 +1,9 @@
 package class26;
 
 public class BankMainClass {
+    public static void main(String[] args) {
+        Bank acc1 = new Bank();
+        Bank acc2 = new Bank("Devx", "123");
+
+    }
 }
