be683ca766789bfd4b27efc58876558999fc7732
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-18 21:40:40 +0300

    Update Bank.java

--- src/main/java/class26/Bank.java
+++ src/main/java/class26/Bank.java
@@ -1,4 +1,34 @@
 package class26;
 
 public class Bank {
+
+    double balance;
+    String accName;
+    String accNumber;
+
+    // Parameterized Constructor
+    Bank(String accNameFromUser, String accNumFromUser){
+        balance = 0;
+        accName = accNameFromUser;
+        accNumber = accNumFromUser;
+    }
+
+    Bank(){
+        System.out.println("From No Arg Constructor");
+    }
+
+
+    // Methods
+    void deposit(int amount) {
+        System.out.println(accName + " is trying to deposit "
+                + amount + " into " + accNumber);
+        balance = balance + amount;
+    }
+
+    void withdraw(int amount) {
+        System.out.println(accName + " is trying to withdraw "
+                + amount + " from " + accNumber);
+        balance = balance - amount;
+    }
+
 }
