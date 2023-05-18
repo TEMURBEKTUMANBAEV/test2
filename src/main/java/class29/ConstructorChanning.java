c35c7a8e84978ed2663d8894a4917771196c72d0
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-18 21:44:22 +0300

    Update ConstructorChanning.java

--- src/main/java/class29/ConstructorChanning.java
+++ src/main/java/class29/ConstructorChanning.java
@@ -1,4 +1,22 @@
 package class29;
+class Vehicle{
+    Vehicle(){
+        System.out.println("From Vehicle");
+    }
+}
+
+class Car extends Vehicle{
+
+}
+
+class SportsCar extends Car{
+    SportsCar(){
+        System.out.println("From SportsCar");
+    }
+}
 
 public class ConstructorChanning {
+    public static void main(String[] args) {
+        SportsCar sportsCar = new SportsCar();
+    }
 }
