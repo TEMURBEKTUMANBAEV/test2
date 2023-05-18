d581fb65b3bb9f510a2b11d58520f96d0690a27b
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-18 21:42:10 +0300

    Update StaticExample.java

--- src/main/java/class26/StaticExample.java
+++ src/main/java/class26/StaticExample.java
@@ -1,4 +1,14 @@
 package class26;
 
+class Car {
+    static int price;
+
+    Car() {
+        price++;
+    }
+}
 public class StaticExample {
+    public static void main(String[] args) {
+        System.out.println(Car.price);
+    }
 }
