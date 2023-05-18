fb71f0bef00a4d05abb1138578088015d899ba90
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-18 21:55:10 +0300

    Update ExampleOfAbstactClass.java

--- src/main/java/class33/ExampleOfAbstactClass.java
+++ src/main/java/class33/ExampleOfAbstactClass.java
@@ -1,4 +1,31 @@
 package class33;
+abstract class Bank {
+
+    int balance;
+
+
+    Bank() {
+
+    }
+
+    public abstract void displayRateOfInterest(); // Abstract method
+
+    public void withdraw(int amount) {
+        balance = balance - amount;
+    }
+}
+
+class CitiBank extends Bank {
+    @Override
+    public void displayRateOfInterest() {
+
+    }
+}
+
 
 public class ExampleOfAbstactClass {
+    public static void main(String[] args) {
+        //Bank b = new Bank(); // We cannot create object of abstract class
+        //b .displayRateOfInterest();
+    }
 }
