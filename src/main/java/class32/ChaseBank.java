0647ecc1d85672325e933247f648bb4faa7bd6d8
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-18 21:53:55 +0300

    Update ChaseBank.java

--- src/main/java/class32/ChaseBank.java
+++ src/main/java/class32/ChaseBank.java
@@ -1,4 +1,22 @@
 package class32;
 
 public class ChaseBank {
+    protected static int balance;
+
+    protected static void withdraw(int amount) {
+        balance = balance - amount;
+    }
+
+    protected static void deposit(int amount) {
+        balance = balance + amount;
+    }
+}
+
+class PurposeAccessModifier {
+    public static void main(String[] args) {
+        ChaseBank cb = new ChaseBank();
+        cb.deposit(5000);
+        cb.balance = 900000000;
+
+    }
 }
