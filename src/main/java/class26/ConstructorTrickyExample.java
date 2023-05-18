bbaaf6e2986d95a09b988e5c2e650ed905a03994
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-18 21:41:31 +0300

    Update ConstructorTrickyExample.java

--- src/main/java/class26/ConstructorTrickyExample.java
+++ src/main/java/class26/ConstructorTrickyExample.java
@@ -1,4 +1,17 @@
 package class26;
 
+class Demo {
+    Demo() { // Constructor
+        System.out.println("From Constructor");
+    }
+
+    void Demo() { // Method
+        System.out.println("From Method");
+    }
+}
 public class ConstructorTrickyExample {
+    public static void main(String[] args) {
+        Demo d = new Demo();
+        d.Demo();
+    }
 }
