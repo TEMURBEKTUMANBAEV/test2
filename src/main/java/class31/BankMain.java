9196c74a4ed704f6bdd5b6689bde33f8c635ae06
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-18 21:51:47 +0300

    Update BankMain.java

--- src/main/java/class31/BankMain.java
+++ src/main/java/class31/BankMain.java
@@ -1,5 +1,14 @@
 package class31;
 
 public class BankMain {
-    
+    public static void main(String[] args) {
+
+        ChaseBank chiragCB = new ChaseBank();
+        CitiBank chiragCiti = new CitiBank();
+
+        Paypal chiragPaypal = new Paypal();
+
+        chiragPaypal.displayRateOfInterest(chiragCB);
+        chiragPaypal.displayRateOfInterest(chiragCiti);
+    }
 }
