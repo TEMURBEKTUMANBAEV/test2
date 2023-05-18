faa5271d5f49af20d991e3e29b5e83fcb0a1211a
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-18 21:48:51 +0300

    Update MethodOverloadingExample.java

--- src/main/java/class30/MethodOverloadingExample.java
+++ src/main/java/class30/MethodOverloadingExample.java
@@ -1,4 +1,18 @@
 package class30;
+class Bank {
 
+    void displayRateOfInterest(int amount) {
+        System.out.println(amount);
+    }
+
+    void displayRateOfInterest(String amount) {
+        System.out.println(amount);
+    }
+}
 public class MethodOverloadingExample {
+    public static void main(String[] args) {
+        Bank b = new Bank();
+        b.displayRateOfInterest(100);
+    }
+
 }
