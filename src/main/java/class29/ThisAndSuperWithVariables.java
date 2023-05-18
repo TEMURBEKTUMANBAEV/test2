995cd334042213833e4dc99cf8a3794144ef06f5
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-18 21:46:39 +0300

    Update ThisAndSuperWithVariables.java

--- src/main/java/class29/ThisAndSuperWithVariables.java
+++ src/main/java/class29/ThisAndSuperWithVariables.java
@@ -1,4 +1,21 @@
 package class29;
+class Parent {
+    String name = "Azat";
+}
 
+class Child extends Parent{
+    String name = "Chirag";
+
+    void printName(){
+        String name = "Maxim";
+        System.out.println(name);
+        System.out.println(this.name);
+        System.out.println(super.name);
+    }
+}
 public class ThisAndSuperWithVariables {
+    public static void main(String[] args) {
+        Child c = new Child();
+        c.printName();
+    }
 }
