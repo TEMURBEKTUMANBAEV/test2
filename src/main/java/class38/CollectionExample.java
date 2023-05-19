1b1a297d6b959c004e76e558431d3c86422aa2af
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-19 08:59:50 +0300

    Create CollectionExample.java

---
+++ src/main/java/class38/CollectionExample.java
@@ -0,0 +1,27 @@
+package class38;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionExample {
+    public static void main(String[] args) {
+        List<Integer> listOfNum = new ArrayList<>();
+        listOfNum.add(40);
+        listOfNum.add(34);
+        listOfNum.add(65);
+        listOfNum.add(32);
+        listOfNum.add(2, 50);
+
+        System.out.println(listOfNum.contains(40));//true
+        System.out.println(listOfNum.isEmpty());
+
+
+        Collections.sort(listOfNum);
+        Collections.reverse(listOfNum);
+        System.out.println(listOfNum);
+
+
+
+    }
+}
