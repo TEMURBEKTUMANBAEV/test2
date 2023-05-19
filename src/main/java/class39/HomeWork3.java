ade865c78b51a29166c944148744aa83fcb4ffb2
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-19 09:05:54 +0300

    Create HomeWork3.java

---
+++ src/main/java/class39/HomeWork3.java
@@ -0,0 +1,25 @@
+package class39;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class HomeWork3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Set<String> setOfWords = new HashSet<>();
+
+
+        System.out.println("Enter any String");
+        String str = scanner.nextLine().replace(",", "");
+
+        String[] arrayOfWords = str.split(" ");
+
+        for (int i = 0; i < arrayOfWords.length; i++) {
+            if (!setOfWords.add(arrayOfWords[i])) {
+                setOfWords.remove(arrayOfWords[i]);
+            }
+        }
+        System.out.println(setOfWords);
+    }
+}
