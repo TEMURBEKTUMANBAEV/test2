fe5b21a32297262dc28d391b3b306218f8b82fd8
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-19 09:08:21 +0300

    Create example1.java

---
+++ src/main/java/class41/example1.java
@@ -0,0 +1,16 @@
+package class41;
+
+public class example1 {
+    public static void main(String[] args) {
+      int number = 10;
+      int a = 0;
+      int b = 1;
+      for(int i = 1; i < number; i++) {
+          int fibonacci = a+b;
+          a = b;
+          b = fibonacci;
+          System.out.println(fibonacci);
+      }
+
+    }
+}
