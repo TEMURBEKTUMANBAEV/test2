9c861faabe4f64cd4523a459beeb2ca890b64957
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-19 08:58:08 +0300

    Create ExceptionRealTime.java

---
+++ src/main/java/class37/ExceptionRealTime.java
@@ -0,0 +1,28 @@
+package class37;
+
+public class ExceptionRealTime {
+    public static void main(String[] args) throws Exception {
+        method1();
+    }
+    static void method1(){
+        method2();
+    }
+
+    static void method2(){
+        method3();
+    }
+
+    static void method3() {method3();} {
+        try {
+            method4();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static void method4() throws ClassNotFoundException {
+
+    }
+
+
+}
