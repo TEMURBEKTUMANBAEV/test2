8cad45cd13bc980f1de72b43f90999c17622c98b
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-19 09:18:56 +0300

    Create class13three.java

---
+++ src/main/java/HomeWork/class13three.java
@@ -0,0 +1,11 @@
+package HomeWork;
+
+public class class13three {
+    public static void main(String[] args) {
+        int num = 1;
+        while (num < 20){
+            System.out.println(num);
+            num+=2;
+        }
+    }
+}
