77fad3351a3190ae1670c8e2bc10865cd7e93dc9
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-19 08:55:38 +0300

    Update ExceptionExample.java

--- src/main/java/class36/ExceptionExample.java
+++ src/main/java/class36/ExceptionExample.java
@@ -1,5 +1,21 @@
 package class36;
 import java.util.Scanner;
 public class ExceptionExample {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter two numbers");
+        int numbers[] = {2, 5, 7, 8};
+
+        try {
+            int num1 = input.nextInt(), num2 = input.nextInt();
+            System.out.println(num1 / num2);
+            System.out.println(numbers[4]);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Welcome to the Party");
+        System.out.println("Enjoy your dinner");
+    }
+
 }
