3dfad557103388942846082da5218d61e703aae6
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-19 09:01:12 +0300

    Create ListExample.java

---
+++ src/main/java/class38/ListExample.java
@@ -0,0 +1,26 @@
+package class38;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExample {
+    public static void main(String[] args) {
+        List<Integer> listOfNum = new ArrayList<>();
+        listOfNum.add(40);
+        listOfNum.add(34);
+        listOfNum.add(65);
+        listOfNum.add(32);
+        listOfNum.add(2, 50);
+
+        System.out.println(listOfNum);
+
+        //get data form the list
+        for (int i = 0; i < listOfNum.size(); i++)   {
+            System.out.println(listOfNum.get(i));
+        }
+
+        for (int data : listOfNum) {
+            System.out.println(data);
+        }
+    }
+}
